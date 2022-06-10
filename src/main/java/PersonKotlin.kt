40bd0b82606442e23ee7e268325755380a21d36a
hweiyu2002 <hweiyu1001@gmail.com>
2022-06-10 16:17:22 +0800

    Hello world with Kotlin

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package hweiyu.com
+
+class PersonKotlin {
+    fun greeting(){
+        println("hello kotlin")
+    }
+}
+
+//Top-level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
