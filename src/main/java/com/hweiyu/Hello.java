262867adf632f56e1eb027a31b67bcf6a4917c45
hweiyu2002 <hweiyu1001@gmail.com>
2022-06-10 14:58:49 +0800

    initial commit

--- src/main/java/com/hweiyu/Hello.java
+++ src/main/java/com/hweiyu/Hello.java
@@ -1,4 +1,9 @@
 package com.hweiyu;
 
 public class Hello {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.greeting();
+        //System.out.println("Hello world");
+    }
 }
